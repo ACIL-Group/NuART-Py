8f605e3425ecf202f7d9fa5302f5699c1f9c4a5c
Islam Elnabarawy <ie3md@mst.edu>
2018-03-21 10:29:40 -0500

    Simplify importing FuzzyART

--- nuart/clustering/__init__.py
+++ nuart/clustering/__init__.py
@@ -13,5 +13,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+from nuart.clustering.fuzzy_art import FuzzyART
 
 __author__ = "Islam Elnabarawy"
