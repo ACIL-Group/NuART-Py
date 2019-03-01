cec738d2c4d8d87f09014b2f48941b10f5abac58
Islam Elnabarawy <ie3md@mst.edu>
2019-02-28 18:16:04 -0600

    Update copyright notice

--- nuart/biclustering/__init__.py
+++ nuart/biclustering/__init__.py
@@ -1,5 +1,5 @@
 """
-   Copyright 2018 Islam Elnabarawy
+   Copyright 2019 Islam Elnabarawy
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
