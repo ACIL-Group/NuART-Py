36011432f6da9b87452c25cb1911a742f353bc49
Islam Elnabarawy <ie3md@mst.edu>
2019-11-03 22:01:06 -0800

    Bump version number to 0.0.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='nuart',
-    version='0.0.4',
+    version='0.0.5',
     install_requires=['numpy', 'scikit-learn', 'scipy'],
     author="Islam Elnabarawy",
     author_email="islam.ossama@gmail.com",
